ee1cac25fa14497141ee9d244afdee6bde6028aa
ChabriGanesh <chabriganesh716@gmail.com>
2025-11-11 00:52:28 +0530

    Fix unused ReactDatePicker import error

--- student-frontend/src/StudentsPage.js
+++ student-frontend/src/StudentsPage.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { CSVLink } from 'react-csv';
-import ReactDatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { Bar } from 'react-chartjs-2';
 import {
